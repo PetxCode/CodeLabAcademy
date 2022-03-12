054d1eb99813a22098cc5a9e339bd4c40fe2717c
PetxCode <71887594+PetxCode@users.noreply.github.com>
2022-03-12 06:15:20 -0800

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose")
 const url = "mongodb://localhost/academyDB"
 const dotenv = require('dotenv')
 dotenv.config()
-
+const url22 = "mongodb+srv://AuthClass:AuthClass@codelab.u4drr.mongodb.net/CodeLabAcademy?retryWrites=true&w=majority"
 const url2 = process.env.DATABASE
 
-mongoose.connect(url2).then(() => {
+mongoose.connect(url22).then(() => {
     console.log("database is now Connected, let's get ready...!")
 })
 
